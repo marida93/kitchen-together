855d9bf20db427513dab4fc6c740cca54e9e518f
marida93 <mariadagre93@gmail.com>
2026-08-15 19:34:56 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
 function sayHello() {
     alert("Welcome to Kitchen Together! 🥰");
 }
+
+function addIngredient() {
+
+    const input = document.getElementById("ingredientInput");
+    const ingredient = input.value;
+
+    if (ingredient === "") {
+        return;
+    }
+
+    const list = document.getElementById("ingredientList");
+
+    const newItem = document.createElement("li");
+
+    newItem.textContent = ingredient;
+
+    list.appendChild(newItem);
+
+    input.value = "";
+}
